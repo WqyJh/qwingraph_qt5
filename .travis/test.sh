3bf683125e7b2e99516001f558e00965feb354c0
WqyJh <781345688@qq.com>
2019-08-20 21:39:17 +0800

    Fix travis

--- .travis/test.sh
+++ .travis/test.sh
@@ -3,6 +3,7 @@
 docker run --security-opt seccomp:unconfined -it \
 -v $TRAVIS_BUILD_DIR:/tmp/qwingraph_qt5 \
 -v $ARTIFACTS:$ARTIFACTS \
+-e ARTIFACTS=$ARTIFACTS \
 ${IMAGE} sh -c 'cd /tmp/qwingraph_qt5; ./test.sh'
 
 exit $?
