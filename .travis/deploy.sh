0b8177ed0aea8788a8b7ec45636790638551205a
WqyJh <781345688@qq.com>
2019-08-20 21:13:05 +0800

    Fix travis

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt-get update && \
-sudo apt-get install -y python3 python3-pip && \
+sudo apt-get install -y python3 python3-setuptools python3-pip && \
 sudo pip3 install githubot && \
 githubot --token ${GH_TOKEN} \
 --repo ${TRAVIS_REPO_SLUG} \
