f7ed730861925cf92f7b777c32d8a12a4a3e660c
WqyJh <781345688@qq.com>
2019-08-20 19:47:06 +0800

    Change another way for deployment

---
+++ .travis/deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+apt-get install python3 python3-pip && \
+pip3 install githubot && \
+githubot --token ${GH_TOKEN} \
+--repo ${TRAVIS_REPO_SLUG} \
+--tag ${TRAVIS_TAG} \
+--title ${TRAVIS_TAG} \
+--message "Release created by githubot" \
+--assets ${ARTIFACTS}/*
