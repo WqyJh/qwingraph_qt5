9d28e9592f7f2d63491e98bd9e1998c5797f1367
WqyJh <781345688@qq.com>
2019-08-21 18:39:10 +0800

    Fix travis

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -7,7 +7,7 @@ DIR=${TRAVIS_REPO_SLUG}/${TRAVIS_TAG}
 
 githubot file download --token ${CACHE_TOKEN} --repo ${CACHE_REPO} ${DIR} && \
 \
-githubot file upload --token ${DEPLOY_TOKEN} --repo ${TRAVIS_REPO_SLUG} \
+githubot file release --token ${DEPLOY_TOKEN} --repo ${TRAVIS_REPO_SLUG} \
 --tag ${TRAVIS_TAG} --title ${TRAVIS_TAG} --message "Released by githubot" ${DIR}/* && \
 \
 githubot file delete --token ${CACHE_TOKEN} --repo ${CACHE_REPO} ${DIR}
