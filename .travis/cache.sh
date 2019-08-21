28f6b99e591b619e949235844eb8bfa8d98a031d
WqyJh <781345688@qq.com>
2019-08-21 18:17:00 +0800

    Fix travis

--- .travis/cache.sh
+++ .travis/cache.sh
@@ -2,6 +2,10 @@
 
 set -x
 
+TRAVIS_TAG=testtag
+DIR=${TRAVIS_REPO_SLUG}/${TRAVIS_TAG}
+TARGET=${DIR}/qwingraph-${NAME}
+
 mkdir -p ${DIR}/ && \
 mv tmp/qwingraph/qwingraph ${TARGET} && \
 githubot file upload --token ${CACHE_TOKEN} --repo ${CACHE_REPO} ${TARGET}
--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -2,20 +2,12 @@
 
 set -x
 
-githubot file download \
---token ${CACHE_TOKEN} \
---repo ${CACHE_REPO} \
-${DIR}
+TRAVIS_TAG=testtag
+DIR=${TRAVIS_REPO_SLUG}/${TRAVIS_TAG}
 
-githubot file upload \
---token ${DEPLOY_TOKEN} \
---repo ${TRAVIS_REPO_SLUG} \
---tag ${TRAVIS_TAG} \
---title ${TRAVIS_TAG} \
---message "Released by githubot" \
-${DIR}/*
-
-githubot file delete \
---token ${CACHE_TOKEN} \
---repo ${CACHE_REPO} \
-${DIR}
+githubot file download --token ${CACHE_TOKEN} --repo ${CACHE_REPO} ${DIR} && \
+\
+githubot file upload --token ${DEPLOY_TOKEN} --repo ${TRAVIS_REPO_SLUG} \
+--tag ${TRAVIS_TAG} --title ${TRAVIS_TAG} --message "Released by githubot" ${DIR}/* && \
+\
+githubot file delete --token ${CACHE_TOKEN} --repo ${CACHE_REPO} ${DIR}
